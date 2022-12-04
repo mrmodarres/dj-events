86df4f4d092e4b50618e1aa7be5a7df895abb849
mehdi modarres <mr.modarres1994@gmail.com>
2022-12-04 11:10:38 +0330

    + react-icons package +modula alias + alis to all components and page + 404 page

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
-import styles from "../styles/Layout.module.css";
+import styles from "@/styles/Layout.module.css";
 export default function Layout({ title, keywords, description, children }) {
   return (
     <div>
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,11 @@
 import React from "react";
 import { useRouter } from "next/router";
-import Layout from "../../components/Layout";
+import Layout from "@/components/Layout";
 function Events() {
   const router = useRouter();
   console.log(router);
   return (
-    <Layout title={router.query.slug} >
+    <Layout title={router.query.slug}>
       <h1>My Events is {router.query.slug}</h1>
     </Layout>
   );
