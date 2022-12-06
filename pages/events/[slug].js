6db4450c2d4b2bcf7173cde6c455ca2f58e21527
mehdi modarres <mr.modarres1994@gmail.com>
2022-12-06 16:04:43 +0330

    + Event structure + Event style

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,47 @@
 import React from "react";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
 import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
+import Image from "next/image";
 import { API_URL } from "config";
+import styles from "@/styles/Event.module.css";
+import Link from "next/link";
 function Events({ evt }) {
+  const deleteEvent = (e) => {
+    console.log(e);
+  };
   return (
     <Layout title={evt.slug}>
-      <h1>My Events is {evt.slug}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/event/edit/${evt.id}`}>
+            <FaPencilAlt /> Edit Event
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes />
+            Delete Event
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} alt={evt.slug} height={600} />
+          </div>
+        )}
+        <h3>Perfomrd:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.desciption}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events" className={styles.back}>
+          Go back
+        </Link>
+      </div>
     </Layout>
   );
 }
