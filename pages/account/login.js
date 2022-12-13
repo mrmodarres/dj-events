186cbd5185f686380a9a6a37e7ae065118c92698
mehdi modarres <mr.modarres1994@gmail.com>
2022-12-13 17:54:27 +0330

    + request for login jwt success

--- pages/account/login.js
+++ pages/account/login.js
@@ -6,14 +6,27 @@ import { ToastContainer } from "react-toastify";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "@/Slice/authSlice";
+import { API_URL } from "@/config/index";
+import axios from "axios";
 function Login() {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (email !== "" && password !== "") {
-      dispatch(getUser());
+      // dispatch(getUser());
+      const values = { identifier: email, password: password };
+      console.log(values);
+      const res = await fetch(`${API_URL}/api/auth/local`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      const data = await res.json();
+      console.log(data);
     }
   };
   return (
