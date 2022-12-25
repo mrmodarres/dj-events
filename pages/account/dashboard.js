f5a77b6926dfc6172b600dacf2f89ac042b1b291
mehdi modarres <mr.modarres1994@gmail.com>
2022-12-25 17:08:33 +0330

    + dashboard jsx + DashboardEvent module + Dashboard ,odule

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,7 +1,16 @@
+import Layout from "@/components/Layout";
 import React from "react";
-
-function dashboard() {
-  return <div>hello welcome to dashboard</div>;
+import styles from "@/styles/Dashboard.module.css";
+function dashboard({ events }) {
+  console.log(events);
+  return (
+    <Layout title="User Dashboard">
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Evetns</h3>
+      </div>
+    </Layout>
+  );
 }
 
 export default dashboard;
